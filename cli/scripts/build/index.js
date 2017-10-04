8f235f590e85c447d1f295f5ba7283ab52619154
Raphael Amorim <rapha850@gmail.com>
2017-10-04 02:20:40 -0300

    cli: build script

---
+++ cli/scripts/build/index.js
@@ -0,0 +1,23 @@
+function Build(rootPath, pathWebOS) {
+  const fs = require('fs');
+  const path = require('path');
+  const chalk = require('chalk');
+  const {execSync} = require('child_process');
+
+  let crowBinPath = path.resolve(
+    __dirname,
+    '../../../node_modules/crow-scripts/bin/crow-scripts-webpack.js'
+  );
+
+  console.info(
+    chalk.dim('[react-tv]'),
+    `bundling...`
+  )
+
+  execSync(
+    `cd ${rootPath} && node ${crowBinPath} --enable-loose --disable-css --disable-manifest --target-browsers webos/bundle/app src`
+  );
+  execSync(`cd ${pathWebOS}/bundle && mv app.*.js app.js && mv app.*.js.map app.js.map`);
+}
+
+module.exports = Build;
