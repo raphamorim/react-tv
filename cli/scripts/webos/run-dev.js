80fe052aca253f75c13625193d798fb70eec91cf
Raphael Amorim <rapha850@gmail.com>
2017-11-03 12:24:26 -0200

    cli: add root node_modules

--- cli/scripts/webos/run-dev.js
+++ cli/scripts/webos/run-dev.js
@@ -8,15 +8,22 @@ const express = require('express'),
 
 module.exports = function _runWebOSDev(pathArg) {
   const pathWebOS = path.resolve(pathArg, 'webos');
+  const pathNodeModules = path.resolve(pathArg, 'node_modules');
 
   // build(pathArg, pathWebOS);
 
-  app.use(express.static(pathWebOS));
+  app.use('/', express.static(pathWebOS));
+  app.use('/node_modules', express.static(pathNodeModules));
 
-  app.listen(port, hostname, () =>
+  app.listen(port, hostname, () => {
+    console.info(
+      chalk.dim('[react-tv]'),
+      `using ${chalk.blue(pathNodeModules)} as ${chalk.blue('node_modules')} alias`
+    )
+
     console.info(
       chalk.dim('[react-tv]'),
       `running on ${chalk.yellow(`${hostname}:${port}`)}`
     )
-  );
+  })
 };
