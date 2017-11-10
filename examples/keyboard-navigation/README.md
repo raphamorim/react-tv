2c9cea204ec5040fba7597a45e29eb892b160ded
Raphael Amorim <rapha850@gmail.com>
2017-11-10 01:49:56 -0200

    renderer: start WebOSSimulator and keyboard-navigation example

---
+++ examples/keyboard-navigation/README.md
@@ -0,0 +1,27 @@
+# Clock App with React-TV as Renderer
+
+Clone the repository:
+
+```shell
+git clone https://github.com/raphamorim/react-tv.git
+```
+
+Open this folder:
+
+```shell
+cd examples/clock-app-with-react-tv
+```
+
+To install:
+
+```shell
+yarn
+```
+
+To run it:
+
+```shell
+yarn start
+```
+
+![Screenshot](screenshot.png)
---
+++ examples/keyboard-navigation/webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/Keyboard.js',
+  output: { path: __dirname, filename: 'bundle.js' },
+  resolveLoader: {
+    root: path.join(__dirname, 'node_modules')
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  },
+};
