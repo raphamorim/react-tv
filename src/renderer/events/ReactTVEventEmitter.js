e459ec3d64e85fd3c2ce8f2a7e48bcba5dd73c92
Raphael Amorim <rapha850@gmail.com>
2017-12-01 00:13:29 -0200

    renderer: support onPress handler

--- src/renderer/events/ReactTVEventEmitter.js
+++ src/renderer/events/ReactTVEventEmitter.js
@@ -58,7 +58,7 @@ export function listenTo(registrationName, mountAt, handler) {
     )
   ) {
     if (registrationName === 'onScroll') {
-      trapCapturedEvent('onScroll', 'scroll', mountAt);
+      trapCapturedEvent('onScroll', 'scroll', mountAt, handler);
     } else if (
       registrationName === 'onFocus' ||
       registrationName === 'onBlur'
@@ -72,6 +72,15 @@ export function listenTo(registrationName, mountAt, handler) {
       // to make sure blur and focus event listeners are only attached once
       isListening.blur = true;
       isListening.focus = true;
+    } else if (registrationName === 'onPress') {
+      trapCapturedEvent('onPress', 'keypress', mountAt,
+        (e) => {
+          // TODO: Separate this logic
+          if (e.keyCode == 13) {
+            handler()
+          }
+        }
+      );
     } else if (EventConstants.hasOwnProperty(registrationName)) {
       trapBubbledEvent(registrationName, dependency, mountAt, handler);
     }
