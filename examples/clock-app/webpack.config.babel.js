8b1c4b14ebeccfeb867247f092ee305941144749
Raphael Amorim <rapha850@gmail.com>
2017-12-06 02:54:02 -0200

    cli: stop run Uglify on 'start-dev', closes #82

--- examples/clock-app/webpack.config.babel.js
+++ examples/clock-app/webpack.config.babel.js
@@ -4,7 +4,7 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const sourcePath = path.join(__dirname, 'src');
 
-module.exports = {
+const config = {
   entry: ['babel-polyfill', path.resolve(sourcePath, 'App.js')],
   output: {
      path: __dirname,
@@ -29,12 +29,20 @@ module.exports = {
       }
     ]
   },
-  plugins: [
+  plugins: []
+}
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin()
+  )
+  config.plugins.push(
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-    new webpack.optimize.ModuleConcatenationPlugin(),
-    new UglifyJsPlugin(),
-    new webpack.HashedModuleIdsPlugin(),
-  ]
+    })
+  )
+  config.plugins.push(new webpack.optimize.ModuleConcatenationPlugin())
+  config.plugins.push(new webpack.HashedModuleIdsPlugin())
 }
+
+module.exports = config;
