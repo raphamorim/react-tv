59fb01e0fca4fe1c80f681e9dbf0aa4cb5112c88
Raphael Amorim <rapha850@gmail.com>
2017-12-08 11:05:25 -0200

    renderer: support style prop

---
+++ src/renderer/__tests__/ReactTVStyle-test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactTVRenderer from '../ReactTVFiberEntry';
+
+const render = ReactTVRenderer.render;
+
+describe('[render] Style Map tests', () => {
+  it('should transform style object into inline style', () => {
+    const style = {
+      div: {
+        fontSize: 19,
+        fontFamily: ['Helvetica', 'arial'],
+        backgroundColor: 'blue',
+        border: '1px solid yellow',
+      },
+      paragraph: {
+        display: 'flex',
+        opacity: 0.5,
+        zIndex: 99,
+      },
+      empty: {},
+    };
+
+    const inlineStyle = {
+      div:
+        'font-size: 19px; font-family: Helvetica,arial; background-color: blue; border: 1px solid yellow;',
+      paragraph: 'display: flex; opacity: 0.5; z-index: 99;',
+    };
+
+    const root = document.createElement('div');
+    const ReactElement = (
+      <div style={style.div}>
+        <div style={style.empty}>
+          <p style={style.paragraph}>cowboy bebop</p>
+        </div>
+      </div>
+    );
+
+    const expectedElement = document.createElement('div');
+    expectedElement.setAttribute('style', inlineStyle.div);
+    const emptyElement = expectedElement.appendChild(
+      document.createElement('div')
+    );
+    const paragraphElement = emptyElement.appendChild(
+      document.createElement('p')
+    );
+    paragraphElement.setAttribute('style', inlineStyle.paragraph);
+
+    paragraphElement.textContent = 'cowboy bebop';
+
+    expect(render(ReactElement, root)).toEqual(expectedElement);
+  });
+});
---
+++ src/renderer/shared/dangerousStyleValue.js
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2017-present, Raphael Amorim.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import {isUnitlessNumber} from './CSSProperty';
+
+/**
+ * Convert a value into the proper css writable value. The style name `name`
+ * should be logical (no hyphens), as specified
+ * in `CSSProperty.isUnitlessNumber`.
+ *
+ * @param {string} name CSS property name such as `topMargin`.
+ * @param {*} value CSS property value such as `10px`.
+ * @return {string} Normalized style value with dimensions applied.
+ */
+function dangerousStyleValue(name, value, isCustomProperty) {
+  const isEmpty = value == null || typeof value === 'boolean' || value === '';
+  if (isEmpty) {
+    return '';
+  }
+
+  if (
+    !isCustomProperty &&
+    typeof value === 'number' &&
+    value !== 0 &&
+    !(isUnitlessNumber.hasOwnProperty(name) && isUnitlessNumber[name])
+  ) {
+    return value + 'px'; // Presumes implicit 'px' suffix for unitless numbers
+  }
+
+  return ('' + value).trim();
+}
+
+export default dangerousStyleValue;
