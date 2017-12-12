1405d5d040e48f67027022e33edbbab432cbac9a
Raphael Amorim <rapha850@gmail.com>
2017-12-12 01:45:28 -0200

    cli: remove novacom error

--- packages/react-tv-cli/scripts/webos/index.js
+++ packages/react-tv-cli/scripts/webos/index.js
@@ -1,9 +1,7 @@
 const run = require('./run');
 const setupDevice = require('node-webos/bin/ares-setup-device.js');
-const novacom = require('node-webos/bin/ares-novacom.js');
 
 module.exports = {
   run,
   setupDevice,
-  novacom,
 };
