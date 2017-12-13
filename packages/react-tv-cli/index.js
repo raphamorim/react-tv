02a28ed9b81d5d5963f22da2cfc4b97651e4b049
Raphael Amorim <rapha850@gmail.com>
2017-12-13 00:50:44 -0200

    cli: get-key-webos

--- packages/react-tv-cli/index.js
+++ packages/react-tv-cli/index.js
@@ -8,10 +8,12 @@ if (argv.length < 2) {
   return help();
 }
 
+let appName;
+let device;
+
 const command = argv[2];
 switch (command) {
   case 'init':
-    let appName;
     if (argv.length > 3) {
       appName = argv[3];
     }
@@ -20,7 +22,6 @@ switch (command) {
     break;
 
   case 'run-webos':
-    let device;
     if (argv.length > 3) {
       device = argv[3];
     }
@@ -30,6 +31,14 @@ switch (command) {
 
   case 'setup-webos':
     WebOS.setupDevice();
+    break;
+
+  case 'get-key-webos':
+    if (argv.length > 3) {
+      device = argv[3];
+    }
+
+    WebOS.getKey(device);
     break;
 
   case '--version':
