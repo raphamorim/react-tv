82ef14385ec803c89e70a76ef3262125eabf89b5
Raphael Amorim <rapha850@gmail.com>
2018-01-04 23:56:01 -0200

    fixup! renderer: remove navigation from renderer

--- examples/navigation/src/App.js
+++ examples/navigation/src/App.js
@@ -1,43 +1,83 @@
 import React from 'react'
+import compose from 'recompose/compose'
+import withStateHandlers from 'recompose/withStateHandlers'
 import ReactTV, { renderOnAppLoaded } from 'react-tv'
 
-class Navigation extends React.Component {
-  componentDidMount() {
-    if (waterfall) {
-      waterfall(document.querySelector('.waterfall'));
-    }
-  }
+let focusKeys = [];
 
-  render() {
-    let items = []
-    for (let i = 1; i <= 10; i++) {
-      const clickHandler = (ev) =>
-        console.log('click ' + i);
-
-      items.push(
-        <div
-          class="item"
-          onFocus={(ev) => console.log('focus ' + i)}
-          onBlur={(ev) => console.log('blur ' + i)}
-          onPress={(ev) => console.log('press ' + i)}
-          onClick={(i !== 2) ? clickHandler : null}
-        >
-          {(i !== 2) ? i : null}
-        </div>
-      )
-    }
-
-    return (
-      <div id='container'>
-        <h1>React-TV Navigation</h1>
-        <div class="waterfall">
-          {items}
-        </div>
-      </div>
-    )
-  }
+const Navigation = {
+  getCurrentFocusKey: () => 'focusKey-2',
+  onFocusChange: () => null,
+  setFocus: (a, b) => {
+    console.log(a, b)
+  },
 }
 
-const App = renderOnAppLoaded(Navigation)
+function isFocused(focusKey) {
+  if (!focusKeys.length) {
+    focusKeys.push(focusKey)
+    // return true;
+  }
+
+  if (focusKey === Navigation.getCurrentFocusKey()) {
+    return true;
+  }
+
+  return false;
+}
+
+function withFocusable(Component, config) {
+  const { focusKey } = config
+  const { setFocus } = Navigation
+  return compose(
+    withStateHandlers(
+      {isFocused: isFocused(focusKey)},
+      {
+        setFocus: ({isFocused}) => (value) => ({
+          isFocused: !isFocused
+        }),
+      }
+    ),
+  )(Component)
+}
+
+function withNavigation(Component) {
+  return compose(
+    withStateHandlers(
+      {
+        currentFocusKey: Navigation.getCurrentFocusKey()
+      },
+      {
+        abc: () => null
+      }
+    )
+  )(Component)
+}
+
+const Item = ({isFocused, setFocus}) => {
+  const focused = (isFocused) ? 'focused' : 'unfocused'
+  return (
+    <div className={focused} onClick={() => setFocus(true)}>
+      It's {focused} Item
+    </div>
+  )
+}
+
+const FocusableItem1 = withFocusable(Item, {focusKey: 'focusKey-1'})
+const FocusableItem2 = withFocusable(Item, {focusKey: 'focusKey-2'})
+const FocusableItem3 = withFocusable(Item, {focusKey: 'focusKey-3'})
+
+function FirstArea({currentFocusKey}) {
+  return (
+    <div>
+      <h1>Current FocusKey: "{currentFocusKey}"</h1>
+      <FocusableItem1/>
+      <FocusableItem2/>
+      <FocusableItem3/>
+    </div>
+  )
+}
+
+const App = renderOnAppLoaded(withNavigation(FirstArea))
 
 ReactTV.render(<App/>, document.querySelector('#root'))
