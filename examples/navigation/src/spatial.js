a6bbbd29b9d3d01f3ab7b9e35df40d2d062afd07
Raphael Amorim <rapha850@gmail.com>
2018-01-15 16:15:30 -0200

    fixup! fixup! navigation: Rename focusKey to focusPath

--- examples/navigation/src/spatial.js
+++ examples/navigation/src/spatial.js
@@ -1,20 +1,15 @@
-const KEYMAPPING = {
-  37: 'left',
-  38: 'up',
-  39: 'right',
-  40: 'down',
-}
+import Navigation from './navigation';
 
 class Spatial {
   constructor() {
-    this.focusPaths = [];
-    this.focused = null;
-    this.setState = null;
-    this.initialized = false;
+    this.focusPaths = []
+    this.focused = null
+    this.setState = null
+    this.initialized = false
 
-    this.handleKeyDown = this.onKeyDown.bind(this)
-
-    this.init()
+    // this.handleKeyDown = this.onKeyDown.bind(this)
+    // this.init()
+    Navigation.init()
   }
 
   withSetState(setFocus) {
@@ -23,13 +18,13 @@ class Spatial {
     }
   }
 
-  init() {
-    document.body.addEventListener('keydown', this.handleKeyDown)
-  }
+  // init() {
+  //   document.body.addEventListener('keydown', this.handleKeyDown)
+  // }
 
-  remove() {
-    document.body.removeEventListener('keydown', this.handleKeyDown)
-  }
+  // remove() {
+  //   document.body.removeEventListener('keydown', this.handleKeyDown)
+  // }
 
   getCurrentFocusedPath() {
     return this.focused
@@ -39,64 +34,67 @@ class Spatial {
     this.focused = focusPath
   }
 
-  focusOnClosest(direction) {
-    direction = KEYMAPPING[direction]
+  // focusOnClosest(direction) {
+  //   direction = KEYMAPPING[direction]
 
-    // when not exist any, focus in first
-    if (!this.focused) {
-      this.focused = this.focusPaths[0]
-      this.setState(this.focusPaths[0])
-      return undefined;
-    }
+  //   // when not exist any, focus in first
+  //   if (!this.focused) {
+  //     this.focused = this.focusPaths[0]
+  //     this.setState(this.focusPaths[0])
+  //     return undefined
+  //   }
 
-    const index = this.focusPaths.indexOf(this.focused)
+  //   const index = this.focusPaths.indexOf(this.focused)
 
-    if (direction === 'right' || direction === 'down') {
-      // should verify direction
-      if ((index + 1) >= this.focusPaths.length) {
-        return undefined;
-      }
+  //   if (direction === 'right' || direction === 'down') {
+  //     // should verify direction
+  //     if ((index + 1) >= this.focusPaths.length) {
+  //       return undefined
+  //     }
 
-      this.focused = this.focusPaths[index + 1]
-      this.setState(this.focusPaths[index + 1])
-      const element = document.querySelector(`#${this.focusPaths[index + 1]}`)
-      console.log(element)
-      element.focus()
-    } else if (direction === 'left' || direction === 'up') {
-      // should verify direction
-      if (index === 0) {
-        return undefined;
-      }
+  //     this.focused = this.focusPaths[index + 1]
+  //     this.setState(this.focusPaths[index + 1])
+  //     const element = document.querySelector(`#${this.focusPaths[index + 1]}`)
+  //     console.log(element)
+  //     element.focus()
+  //   } else if (direction === 'left' || direction === 'up') {
+  //     // should verify direction
+  //     if (index === 0) {
+  //       return undefined;
+  //     }
 
-      this.focused = this.focusPaths[index - 1]
-      this.setState(this.focusPaths[index - 1])
-      const element = document.querySelector(`#${this.focusPaths[index - 1]}`)
-      console.log(element)
-      element.focus()
-    }
-  }
+  //     this.focused = this.focusPaths[index - 1]
+  //     this.setState(this.focusPaths[index - 1])
+  //     const element = document.querySelector(`#${this.focusPaths[index - 1]}`)
+  //     console.log(element)
+  //     element.focus()
+  //   }
+  // }
 
-  onKeyDown(evt) {
-    if (
-      evt.altKey ||
-      evt.ctrlKey ||
-      evt.metaKey ||
-      evt.shiftKey
-    ) {
-      return;
-    }
+  // onKeyDown(evt) {
+  //   if (
+  //     evt.altKey ||
+  //     evt.ctrlKey ||
+  //     evt.metaKey ||
+  //     evt.shiftKey
+  //   ) {
+  //     return;
+  //   }
 
-    this.focusOnClosest(evt.keyCode)
-    return evt.preventDefault();
-  }
+  //   this.focusOnClosest(evt.keyCode)
+  //   return evt.preventDefault()
+  // }
 
   addFocusable(focusPath) {
-    if (this.focusPaths.indexOf(focusPath) > -1) {
-      return undefined;
-    }
+    // if (this.focusPaths.indexOf(focusPath) > -1) {
+    //   return undefined;
+    // }
 
-    this.focusPaths.push(focusPath)
+    // this.focusPaths.push(focusPath)
+    Navigation.add({selector: `#${focusPath}`})
   }
 }
+
+window.Navigation = Navigation
 
 export default Spatial
