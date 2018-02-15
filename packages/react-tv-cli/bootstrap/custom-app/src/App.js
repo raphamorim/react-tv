2b473ad8a5cd5bad18b0c6e684765e7ef60ef682
Wojciech Bilicki <wojciech.bilicki@gmail.com>
2018-02-15 13:11:32 +0100

    Fix #116
    Adding 'https' seems to fix the posters not being visible on the simulator.

--- packages/react-tv-cli/bootstrap/custom-app/src/App.js
+++ packages/react-tv-cli/bootstrap/custom-app/src/App.js
@@ -20,9 +20,9 @@ class App extends React.Component {
         <div className='title'>React-TV Template</div>
         <div className='focus-info'>You're focused on: {this.props.currentFocusPath}</div>
         <div className='posters'>
-          <FocusablePoster focusPath='focusable-poster-1' src={'//upload.wikimedia.org/wikipedia/en/1/15/Dunkirk_Film_poster.jpg'}/>
-          <FocusablePoster focusPath='focusable-poster-2' src={'//upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg'}/>
-          <FocusablePoster focusPath='focusable-poster-3' src={'//upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg'}/>
+          <FocusablePoster focusPath='focusable-poster-1' src={'https://upload.wikimedia.org/wikipedia/en/1/15/Dunkirk_Film_poster.jpg'}/>
+          <FocusablePoster focusPath='focusable-poster-2' src={'https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg'}/>
+          <FocusablePoster focusPath='focusable-poster-3' src={'https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg'}/>
         </div>
       </div>
     );
